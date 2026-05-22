e8b8e7d5ec267ca39bc3a507938d1d5b1a8b5592
MerakiDev <jmercedes.palban@gmail.com>
2026-05-22 04:24:47 +0000

    feat: se implementa estilos unificados, se crea navbar y footer

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -6,7 +6,34 @@ import { PetGalleryComponent } from './features/pet-gallery/pet-gallery.componen
  * Se mapea la ruta raíz al componente de galería para el renderizado inicial.
  */
 export const routes: Routes = [
-  { path: '', component: PetGalleryComponent }, // Al entrar a localhost:4200 carga esto
-  { path: 'gallery', component: PetGalleryComponent }, // localhost:4200/gallery
-  { path: '**', redirectTo: '' } // Cualquier otra cosa, vuelve al inicio
+  { 
+    path: '', 
+    redirectTo: 'mascotas', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'mascotas', 
+    loadComponent: () => import('./features/pet-gallery/pet-gallery.component').then(m => m.PetGalleryComponent) 
+  },
+  { 
+    path: 'conocenos', 
+    loadComponent: () => import('./features/about/about.component').then(m => m.AboutComponent) 
+  },
+  /*{ 
+    path: 'adopcion', 
+    loadComponent: () => import('./features/adoption-form/adoption-form.component').then(m => m.AdoptionFormComponent) 
+  },
+  // Rutas futuras para expandir el sistema MEAN
+  { 
+    path: 'reportes', 
+    loadComponent: () => import('./features/reports/reports.component').then(m => m.ReportsComponent) 
+  },
+  { 
+    path: 'auth', 
+    loadComponent: () => import('./features/auth/login().component').then(m => m.LoginComponent) 
+  },*/
+  { 
+    path: '**', 
+    redirectTo: 'mascotas' 
+  }
 ];
